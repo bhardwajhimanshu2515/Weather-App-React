b64cf3f94a32db0a61db3e22623c0299524a91ec
bhardwajhimanshu2515@gmail.com <bhardwajhimanshu2515@gmail.com>
2020-08-03 00:55:15 +0530

    himanshu | created navbar

---
+++ src/shared/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.css'
+function Navbar(){
+    <div id="navbar">
+        <div id="logo">
+            Weather App
+        </div>
+        <div id="navlinks">
+            <a href="https://github.com/bhardwajhimanshu2515"><span class="fa fa-github"></span></a>
+            <a href="https://www.linkedin.com/in/himanshu-bhardwaj-mern-stack-developer/"><span class="fa fa-linkedin"></span></a>
+        </div>
+    </div>
+}
+export default Navbar;
